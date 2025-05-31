1218a34d59311edfe27d2ac8cee1532c0bf17be9
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-06-01 00:56:28 +0900

    sql文ごとにファイルを分けた

--- psql/users.sql
+++ psql/users.sql
@@ -5,14 +5,3 @@ CREATE TABLE users_table (
     password_hash TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE travel_table(
-    t_id SERIAL PRIMARY KEY,
-    t_title VARCHAR(25) NOT NULL,
-    t_location VARCHR(25) NOT NULL,
-    human_number INTEGER NOT NULL,
-    overview TEXT,
-    u_id INTEGER NOT NULL REFERENCES users_table(id) ON DELETE CASCADE
-    t_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
--- created_atこれは、ユーザーがアカウントを作成した日時を記録します。
