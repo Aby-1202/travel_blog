c21cbad89114c35711c3920ad43654affd2a592c
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-06-03 00:06:39 +0900

    psql文のわからないところに説明を加える

--- psql/travel_data.sql
+++ psql/travel_data.sql
@@ -4,6 +4,9 @@ CREATE TABLE travel_data(
     t_location VARCHR(25) NOT NULL,
     human_number INTEGER NOT NULL,
     overview TEXT,
-    u_id INTEGER NOT NULL REFERENCES users_table(id) ON DELETE CASCADE
+    u_id INTEGER NOT NULL REFERENCES users_table(id) ON DELETE CASCADE 
     t_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
+
+--users_tableのidを入れる、そしてON DELETE CASCADEを使用することによってusers_table の対応ユーザーが削除された場合、この旅行データも一緒に削除される
+--t_created_atここでは、旅行データが作成された日時を記録するためのカラムです。デフォルトで現在のタイムスタンプが設定されます。
