1615deb588ca823bb3740ccce96c3db65637600a
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-06-04 19:18:07 +0900

    データ表示を可能にした

--- routes/home.py
+++ routes/home.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, session
+import sqlite3
 
 home_bp = Blueprint('home', __name__)
 
@@ -11,4 +12,17 @@ def home():
     user_id = session['user_id']
     username = session.get('username', 'ゲスト')
 
-    return render_template('home.html', user_id=user_id, username=username)
+    # travel_data をデータベースから取得
+    conn = sqlite3.connect('app.db')
+    conn.row_factory = sqlite3.Row  # dict型でアクセス可能にする
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT * FROM travel_data
+        WHERE u_id = ?
+    """, (user_id,))
+    travel_data_list = cursor.fetchall()
+
+    conn.close()
+
+    return render_template('home.html', user_id=user_id, username=username, travel_data_list=travel_data_list)
