0c8afde2c444b4f2890f5c58c5a7a005eb23d595
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-06-05 14:56:10 +0900

    テーブルにおいて旅行開始時期と終了時期の記載

--- psql/travel_data.sql
+++ psql/travel_data.sql
@@ -4,9 +4,12 @@ CREATE TABLE travel_data (
     t_location TEXT NOT NULL,
     human_number INTEGER NOT NULL,
     overview TEXT,
+    start_date DATE NOT NULL,  -- 開始日
+    end_date DATE NOT NULL,    -- 終了日
     u_id INTEGER NOT NULL,
     FOREIGN KEY(u_id) REFERENCES users_table(id)
 );
+
 
 --users_tableのidを入れる、そしてON DELETE CASCADEを使用することによってusers_table の対応ユーザーが削除された場合、この旅行データも一緒に削除される
 --t_created_atここでは、旅行データが作成された日時を記録するためのカラムです。デフォルトで現在のタイムスタンプが設定されます。
