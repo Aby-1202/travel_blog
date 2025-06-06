325bf50d33ae0ec59a43cc669f6d75f10ddc0e3c
naaaahah <s2322055@stu.musashino-u.ac.jp>
2025-06-06 16:34:23 +0900

    画像登録動作2

--- routes/input.py
+++ routes/input.py
@@ -50,7 +50,7 @@ def input():
                 return render_template('input.html', username=username)
             
         try:
-            conn = sqlite3.connect('travel_blog/app.db')
+            conn = sqlite3.connect('app.db')
             cursor = conn.cursor()
 
             cursor.execute("""
