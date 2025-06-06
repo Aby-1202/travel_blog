9fb22ba2090d4a5489df828ab1a74cbe6085177d
EgashiraShuta <s2422036@stu.musashino-u.ac.jp>
2025-06-06 17:06:17 +0900

    ホームから詳細確認できるようにした

--- routes/__init__.py
+++ routes/__init__.py
@@ -6,4 +6,4 @@ from.input import input_bp
 from .my_travel import my_travel_bp
 from .users_data import users_data_bp
 from .travel_details_input import travel_details_input_bp
-
+from .detail import detail_bp
