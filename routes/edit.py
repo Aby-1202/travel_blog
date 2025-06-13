d8b79a310763a4ff5e40bf7c8de029372afe5447
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-06-13 16:11:31 +0900

    ミス

--- routes/edit.py
+++ routes/edit.py
@@ -7,14 +7,16 @@ from werkzeug.utils import secure_filename
 
 edit_bp = Blueprint('edit', __name__)
 
-@edit_bp.route('/edit/<int:post_id>', methods=['GET', 'POST'])
-def edit(post_id):
+@edit_bp.route('/edit', methods=['GET', 'POST'])
+def edit():
     if 'user_id' not in session:
         flash("ログインしてください")
         return redirect(url_for('login.login'))
 
     user_id = session['user_id']
     username = session.get('username', 'ゲスト')
+
+    return render_template('edit.html',user_id=user_id, username=username)
 
 
 ##ここから下に前のページのtravel_dataのidを取得して、travel_dataの内容を表示するようなコードを記述する
